1b7ff43cb5a299a2165f6c8d4897f306f2febbf3
Harshvardhan <140024736+harsh9719@users.noreply.github.com>
2026-09-19 11:44:34 +0400

    Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,8 +1,12 @@
 import os
 import sys
+from src.components.data_transformation import DataTransformation
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
@@ -50,4 +54,5 @@ if __name__=="__main__":
     train_data,test_data=obj.initiate_data_ingestion()
 
 
-
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
